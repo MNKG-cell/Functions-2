4a02f0456b445be477136c1c52e38b03ca63c7ee
MAUREEN <maureennayebare2@gmail.com>
2020-11-09 10:02:20 +0300

    Functions 2

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+
+function addNumbers( num1,num2)
+{addNumbers(5, 6);
+console. log('sum');} 
+
+function showDialog(){alert("Hello There!");
+    console. log('alerted');} 
+
